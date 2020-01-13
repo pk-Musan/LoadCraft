e0aaa3e17c8c2e51da86aacdc186f74b0a33c631
pk-Musan <mu.1ok.san@gmail.com>
2020-01-13 15:34:30 +0900

    壊したブロックの入手，所持ブロックの表示まで

--- Player.cpp
+++ Player.cpp
@@ -3,6 +3,8 @@
 #include "NormalBlock.h"
 #include "Loader.h"
 
+#include <string>
+
 Player::Player() : power( 2 ) {
 
 	x = 0.0F;
@@ -35,11 +37,11 @@ void Player::getBlock( int maxDurability, int imageType ) {
 	Block* block = new NormalBlock( 0, 0, maxDurability, imageType );
 	
 	// 同じタイプのブロックを持っていないか探す．
-	for ( std::vector<Block*> blocks : blocksList ) {
-		for ( Block* b : blocks ) {
-			// 同じタイプのブロックがあればその時のvectorに追加する
-			if ( b->getMaxDurability() == block->getMaxDurability() && b->getImageType() == block->getImageType() ) {
-				blocks.push_back( block );
+	int i = 0;
+	for ( i = 0; i < ( int )blocksList.size(); i++ ) {
+		if ( blocksList.at( i ).size() >= 1 ) {
+			if ( blocksList.at( i ).at( 0 )->getMaxDurability() == block->getMaxDurability() && blocksList.at( i ).at( 0 )->getImageType() == block->getImageType() ) {
+				blocksList.at( i ).push_back( block );
 				return;
 			}
 		}
@@ -68,9 +70,7 @@ void Player::moveY( float dy ) {
 }
 
 void Player::draw( float cameraX, float cameraY ) {
-	// 持っているブロックの表示
-
-
+	
 	float X, Y;
 	X = x - cameraX;
 	Y = y - cameraY;
@@ -89,5 +89,19 @@ void Player::draw( float cameraX, float cameraY ) {
 	} else {
 		if ( direction > 0 ) DrawGraph( ( int )( X - size * 0.5F ), ( int )( Y - size * 0.5F ), Loader::imageHandles[Loader::RIGHT], TRUE );
 		else if ( direction < 0 ) DrawGraph( ( int )( X - size * 0.5F ), ( int )( Y - size * 0.5F ), Loader::imageHandles[Loader::LEFT], TRUE );
+	}
+
+	// 持っているブロックの表示
+	DrawBox( 24, 16, 92, 84, GetColor( 255, 255, 255 ), TRUE );
+	DrawBox( 26, 18, 90, 82, GetColor( 0, 0, 0 ), TRUE );
+	DrawTriangleAA( 20.0F, 40.0F, 20.0F, 59.0F, 4.0F, 49.5F, GetColor( 240, 240, 240 ), TRUE );
+	DrawTriangleAA( 96.0F, 40.0F, 96.0F, 59.0F, 112.0F, 49.5F, GetColor( 240, 240, 240 ), TRUE );
+	if ( blocksList.size() >= 1 ) {
+		blocksList.at( selectedItemNum ).at( 0 )->setPos( 42.0F + 32.0F * 0.5F, 34.0F + 32.0F * 0.5F );
+		blocksList.at( selectedItemNum ).at( 0 )->draw( 0.0F, 0.0F, Loader::imageHandles[blocksList.at( selectedItemNum ).at( 0 )->getImageType()] );
+
+		std::string str = std::to_string( ( int )blocksList.at( selectedItemNum ).size() );
+		const char* block_num = str.c_str();
+		DrawFormatString( 61, 65, GetColor( 255, 255, 255 ), "%3s", block_num );
 	}
 }
